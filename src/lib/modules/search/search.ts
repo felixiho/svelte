ffd61b2ea13bca6e76aafc28f418ec054a647715
felixiho <okekefelix1@gmail.com>
2024-05-28 07:28:38 +0100

    added envs

--- src/lib/modules/search/search.ts
+++ src/lib/modules/search/search.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import type { QueryType } from './types';
 
 export const searchUser = async ({ query, page, perPage }: QueryType) => {
-	const response =  await axios.get(
-		`https://api.github.com/search/users?q=${query}&page=${page}&per_page=${perPage}`
+	const response = await axios.get(
+		`${import.meta.env.VITE_GITHUB_API_URL}/search/users?q=${query}&page=${page}&per_page=${perPage}`
 	);
-  return response.data
+	return response.data;
 };
